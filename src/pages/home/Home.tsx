d9b1ad29da7fa0b24264628043bc27dd687d9b26
Gustavo Carvalho <gustavo_allm@outlook.com>
2025-07-24 21:18:39 -0300

    feat: add new ui style

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-full p-8 text-center rounded-md">
-      <h1 className="text-4xl font-bold mb-4">Dev Tool Chest</h1>
+      <h1 className="text-4xl font-bold mb-4 text-primary">Dev Tool Chest</h1>
       <p className="text-lg text-muted-foreground max-w-xl mb-6">
-        Welcome to <span className="font-semibold">Dev Tool Chest</span>!<br />
+        Welcome to <span className="font-semibold text-primary">Dev Tool Chest</span>!<br />
         This is your productivity hub for developers, bringing together several essential tools in one place,
         with a microfrontend architecture for maximum flexibility and scalability.
       </p>
-      <ul className="text-left mx-auto max-w-md list-disc list-inside text-muted-foreground">
+      <ul className="text-left mx-auto max-w-md list-disc list-inside text-primary">
         <li>Quickly view and edit Markdown</li>
         <li>Parse and analyze JSON</li>
         <li>Compare files with the diff viewer</li>
